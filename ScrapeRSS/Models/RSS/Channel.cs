f01ddc687693357407a2295e5772943174ef2b11
UrbanCMC <UrbanCMC@web.de>
2020-02-02 01:22:41 +0100

    Add RSS feed models

---
+++ ScrapeRSS/Models/RSS/Channel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ScrapeRSS.Models.RSS
+{
+    /// <summary>
+    /// Represents the root objects an RSS feed is generated from
+    /// </summary>
+    [XmlRoot("channel")]
+    public class Channel
+    {
+        /// <summary>
+        /// Gets or sets the description
+        /// </summary>
+        [XmlElement("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the feed items
+        /// </summary>
+        [XmlElement("item")]
+        public List<Item> Items { get; set; }
+
+        /// <summary>
+        /// Gets or sets the website URL
+        /// </summary>
+        [XmlElement("link")]
+        public string Link { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title
+        /// </summary>
+        [XmlElement("title")]
+        public string Title { get; set; }
+
+        public bool ShouldSerializeItems() => Items?.Count > 0;
+    }
+}
---
+++ ScrapeRSS/Models/RSS/Item.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace ScrapeRSS.Models.RSS
+{
+    /// <summary>
+    /// Represents a single article in an RSS feed
+    /// </summary>
+    public class Item
+    {
+        /// <summary>
+        /// Gets or sets the author
+        /// </summary>
+        [XmlElement("author")]
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description
+        /// </summary>
+        [XmlElement("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the article URL
+        /// </summary>
+        [XmlElement("link")]
+        public string Link { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the article was published.
+        /// </summary>
+        [XmlElement("pubDate")]
+        public DateTime? PubDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title
+        /// </summary>
+        [XmlElement("title")]
+        public string Title { get; set; }
+
+        public bool ShouldSerializeAuthor() => !string.IsNullOrWhiteSpace(Author);
+
+        public bool ShouldSerializePubDate() => PubDate != null;
+    }
+}
